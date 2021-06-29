53d287e7fbb4fb42f7baa9e0369e1135098898fc
Dang Jinner <nguyenhaidangyq@gmail.com>
2021-06-29 22:26:44 +0700

    update

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Department;
 use App\Models\Staff;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\Support\Facades\Session;
 
@@ -15,114 +16,15 @@ class AdminController extends Controller
 
     }
 
-    public function manageDepartments() {
-        $departments = Department::withCount("Staff")->paginate(20);
-        return view("admin.department.departments", [
-            "departments" => $departments
-        ]);
+    public function adminLogout() {
+        $user = Auth::user();
+        Auth::login($user, false);
+        return Redirect::to("/login");
     }
 
-    public function departmentDetails($department_id) {
-        $department = Department::withCount("Staff")->where("department_id", $department_id)->first();
-        $staffs = Staff::with("Department")->where("department_id",$department_id )->paginate(10);
-        return view("admin.department.department_details", [
-            "department" => $department,
-            "staffs" => $staffs,
-            "edit" => false
-        ]);
-    }
 
-    public function departmentEdit($department_id) {
-        $department = Department::withCount("Staff")->where("department_id", $department_id)->first();
-        $staffs = Staff::with("Department")->where("department_id",$department_id )->paginate(10);
-        return view("admin.department.department_details", [
-            "department" => $department,
-            "staffs" => $staffs,
-            "edit" => true
-        ]);
-    }
 
-    public function departmentUpdate(Request $request, $department_id) {
-        $data = array();
-        $data["department_name"] = $request->get("department_name");
-        $data["department_code"] = $request->get("department_code");
-        $data["department_pic"] = $request->get("department_pic");
-        $data["department_desc"] = $request->get("department_desc");
-        $get_department = Department::findOrFail($department_id);
-        $get_department->update($data);
-        Session::put("message", "Update information department success !!");
-        return Redirect::to("/admin/department-details/".$department_id);
 
-    }
-
-    public function departmentRemoveMmember($staff_id) {
-        try {
-            $staff = Staff::findOrFail($staff_id);
-            $department_id = $staff->department_id;
-            $staff->update([
-                "department_id" => null
-            ]);
-            Session::put("message", "Remove member from department success ");
-            return Redirect::to("/admin/department-details/".$department_id);
-
-        }catch (\Exception $e) {
-            abort(404);
-        }
-    }
-
-    public function departmentAddMember($department_id) {
-        $staffs = Staff::all()->where("department_id", "=", "");
-        return view("admin.department.staffs_list", [
-            "staffs" => $staffs,
-            "department_id" => $department_id
-        ]);
-    }
-
-    public function departmentUpdateMember($department_id, $staff_id) {
-        try {
-            $staff = Staff::findOrFail($staff_id);
-            $staff->update([
-                "department_id" => $department_id
-            ]);
-            Session::put("message", "Add new member to department success !!");
-            return Redirect::to("/admin/department-details/".$department_id);
-        }catch (\Exception $e) {
-            abort(404);
-        }
-
-    }
-
-    public function manageStaffs() {
-        $staffs = Staff::with("Department")->paginate(20);
-        return view("admin.staff.staffs_list", [
-            "staffs" => $staffs
-        ]);
-    }
-
-    public function staffInfo($staff_id) {
-        try {
-            $staff = Staff::findOrFail($staff_id);
-            return view("admin.staff.staff_details", [
-                "staff" => $staff,
-                "edit" => false
-            ]);
-        }catch (\Exception $exception) {
-            dd($exception->getMessage());
-        }
-
-    }
-
-    public function staffEditInfo($staff_id) {
-        try {
-            $staff = Staff::findOrFail($staff_id);
-            return view("admin.staff.staff_details", [
-                "staff" => $staff,
-                "edit" => true
-            ]);
-        }catch (\Exception $exception) {
-            dd($exception->getMessage());
-        }
-    }
 
     public function staffUpdateInfo($staff_id, Request $request) {
         try {
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\StaffController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,24 +17,25 @@ use App\Http\Controllers\AdminController;
 Route::middleware(["auth", "admin"])->group(function () {
     //Admin Route
     Route::get('/', [AdminController::class, "adminDashboard"]);
+    Route::get('/logout', [AdminController::class, "adminLogout"]);
 
     // Department
-    Route::get('/manage-departments', [AdminController::class, "manageDepartments"]);
-    Route::get('/department-details/{department_id}', [AdminController::class, "departmentDetails"]);
-    Route::get('/department-details/edit/{department_id}', [AdminController::class, "departmentEdit"]);
-    Route::post('/department-details/update/{department_id}', [AdminController::class, "departmentUpdate"]);
-    Route::get('/department/remove-member/{staff_id}', [AdminController::class, "departmentRemoveMember"]);
-    Route::get('/department/add-member/{department_id}', [AdminController::class, "departmentAddMember"]);
-    Route::get('/department/{department_id}/update-member/{staff_id}', [AdminController::class, "departmentUpdateMember"]);
+    Route::get('/manage-departments', [DepartmentController::class, "manageDepartments"]);
+    Route::get('/department-details/{department_id}', [DepartmentController::class, "departmentDetails"]);
+    Route::get('/department-details/edit/{department_id}', [DepartmentController::class, "departmentEdit"]);
+    Route::post('/department-details/update/{department_id}', [DepartmentController::class, "departmentUpdate"]);
+    Route::get('/department/remove-member/{staff_id}', [DepartmentController::class, "departmentRemoveMember"]);
+    Route::get('/department/add-member/{department_id}', [DepartmentController::class, "departmentAddMember"]);
+    Route::get('/department/{department_id}/update-member/{staff_id}', [DepartmentController::class, "departmentUpdateMember"]);
 
     //Staff
-    Route::get('/manage-staffs', [AdminController::class, "manageStaffs"]);
-    Route::get('/manage-staffs/{staff_id}/details', [AdminController::class, "staffInfo"]);
-    Route::get('/manage-staffs/{staff_id}/edit', [AdminController::class, "staffEditInfo"]);
-    Route::post('/manage-staffs/{staff_id}/update', [AdminController::class, "staffUpdateInfo"]);
-    Route::get('/manage-staffs/delete/{staff_id}', [AdminController::class, "deleteStaff"]);
-    Route::get('/manage-staffs/add-new-staff', [AdminController::class, "addNewStaff"]);
-    Route::post('/manage-staffs/new-staff', [AdminController::class, "updateNewStaff"]);
+    Route::get('/manage-staffs', [StaffController::class, "manageStaffs"]);
+    Route::get('/manage-staffs/{staff_id}/details', [StaffController::class, "staffInfo"]);
+    Route::get('/manage-staffs/{staff_id}/edit', [StaffController::class, "staffEditInfo"]);
+    Route::post('/manage-staffs/{staff_id}/update', [StaffController::class, "staffUpdateInfo"]);
+    Route::get('/manage-staffs/delete/{staff_id}', [StaffController::class, "deleteStaff"]);
+    Route::get('/manage-staffs/add-new-staff', [StaffController::class, "addNewStaff"]);
+    Route::post('/manage-staffs/new-staff', [StaffController::class, "updateNewStaff"]);
 });
 
 
