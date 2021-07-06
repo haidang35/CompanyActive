b166fa260f4c50b7edcc32e60fdecae4c430bc38
Dang Jinner <nguyenhaidangyq@gmail.com>
2021-07-06 15:33:07 +0700

    update

--- resources/views/admin/document/view_document.blade.php
+++ resources/views/admin/document/view_document.blade.php
@@ -5,10 +5,6 @@
             <div class="col-md-12">
                 <div class="card">
                     <iframe src="https://docs.google.com/viewer?url={{asset("upload/documents/".$document->document_name)}}&embedded=true"></iframe>
-                    <iframe width="1120"
-                            height="850"
-                            src="https://api.aspose.cloud/words/view?foldername=upload/documents&filename={{$document->document_name}}">
-                    </iframe>
                 </div>
             </div>
         </div>
